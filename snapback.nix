e5efeb0839a4d6c69bc7972b6befa401aaaae119
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2018-10-27 19:56:51 -0700

    Basic snapshot creation

--- snapback.nix
+++ snapback.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, directory, filepath, process, stdenv, time
+, unix
+}:
 mkDerivation {
   pname = "snapback";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    base directory filepath process time unix
+  ];
   license = stdenv.lib.licenses.asl20;
 }
