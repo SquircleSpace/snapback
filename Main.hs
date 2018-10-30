ecccbdc2dfcea1dab617cbd8d56dc0ff417966bb
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2018-10-29 17:58:03 -0700

    Move snapshoting into a separate module

--- Main.hs
+++ Main.hs
@@ -1,20 +1,10 @@
 module Main where
-import Control.Exception (bracket, try, IOException)
-import Control.Monad (forM_)
-import Data.Time.Clock (UTCTime, getCurrentTime)
-import Data.Time.Format (formatTime, defaultTimeLocale)
-import System.Directory (createDirectoryIfMissing, removeFile)
-import System.Exit (ExitCode (..))
-import System.FilePath ((</>), (<.>), splitFileName)
-import System.Posix.IO (openFd, closeFd, OpenFileFlags, defaultFileFlags, OpenMode (ReadOnly), exclusive)
-import System.Posix.Types (Fd)
-import System.Process (CreateProcess, proc, readCreateProcessWithExitCode)
+import Snapshot (snapshot, Snapshotable(..), subvolumePath, snapshotBasePath)
 
-data Snapshotable =
-  Snapshotable
-  { subvolumePath :: FilePath
-  , snapshotBasePath :: FilePath
-  }
+import Control.Monad (forM_)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.FilePath ((</>))
 
 standardSnapshotable :: FilePath -> Snapshotable
 standardSnapshotable path =
@@ -29,62 +19,61 @@ subvolumesToSnapshot =
     standardSnapshotable "/media"
   ]
 
-snapshotProcess :: FilePath -> FilePath -> CreateProcess
-snapshotProcess sourcePath destinationPath =
-  proc "btrfs" ["subvolume", "snapshot", "-r", sourcePath, destinationPath]
-
-subpathForTime :: UTCTime -> FilePath
-subpathForTime time =
-  year </> month </> day </> timePart
-  where
-    format :: String -> String
-    format str = formatTime defaultTimeLocale str time
-    year = format "%Y"
-    month = format "%m"
-    day = format "%d"
-    timePart = format "%T"
-
-snapshotDestination :: Snapshotable -> UTCTime -> FilePath
-snapshotDestination snapshotable time = snapshotBasePath snapshotable </> subpathForTime time
-
-mkdirForSnapshotDestination :: FilePath -> IO ()
-mkdirForSnapshotDestination snapshotDestination = do
-  let (containingDir, target) = splitFileName snapshotDestination
-  createDirectoryIfMissing True containingDir
-
-lockfilePathForSnapshot :: FilePath -> FilePath
-lockfilePathForSnapshot snapshotPath = snapshotPath <.> "lock"
-
-withLockFile :: FilePath -> IO a -> IO (Either String a)
-withLockFile lockFile action = do
-  let flags = defaultFileFlags { exclusive = True }
-  let open = try $ openFd lockFile ReadOnly (Just 0) flags :: IO (Either IOException Fd)
-  let close eitherFd = case eitherFd of
-        Right fd -> closeFd fd >> removeFile lockFile
-        Left _ -> return ()
-  bracket open close $ \eitherFd -> case eitherFd of
-    Right _ -> do
-      value <- action
-      return $ Right value
-    Left _ -> return $ Left "Failed to create lockfile"
-
-snapshot :: Snapshotable -> IO (Either String ())
-snapshot snapshotable = do
-  now <- getCurrentTime
-  let destinationPath = snapshotDestination snapshotable now
-  let createProcess = snapshotProcess (subvolumePath snapshotable) destinationPath
-  let processAction = readCreateProcessWithExitCode createProcess ""
-  mkdirForSnapshotDestination destinationPath
-  result <- withLockFile (lockfilePathForSnapshot destinationPath) processAction
-  case result of
-    Left e -> return $ Left e
-    Right (ExitSuccess, _, _) -> return $ Right ()
-    Right (ExitFailure _, stdout, stderr) -> return $ Left stderr
-
-main :: IO ()
-main = do
+snapshotMain :: [String] -> IO ()
+snapshotMain args = do
   forM_ subvolumesToSnapshot $ \subvolume -> do
-    result <- snapshot subvolume
+    result <- takeSnapshot subvolume
     case result of
       Left str -> putStrLn str
       Right _ -> return ()
+
+data Help =
+  Help
+  { brief :: String
+  , detailed :: String
+  }
+
+snapshotHelp :: Help
+snapshotHelp =
+  Help
+  { brief = "Snapshot all configured subvolumes"
+  , detailed = "Snapshot all configured subvolumes"
+  }
+
+data Verb =
+  Verb
+  { name :: String
+  , help :: Help
+  , command :: [String] -> IO ()
+  }
+
+verb name help command = Verb { name = name, help = help, command = command }
+
+verbs :: [Verb]
+verbs =
+  [ verb "snapshot" snapshotHelp snapshotMain
+  ]
+
+performVerb :: String -> [String] -> IO ()
+performVerb verbName args = search verbs
+  where search [] = printUsage >> exitFailure
+        search (currentVerb : restVerbs) =
+          if name currentVerb == verbName
+          then command currentVerb args
+          else search restVerbs
+
+printUsage :: IO ()
+printUsage = do
+  putStrLn "Usage: snapback <command> [args..]"
+  putStrLn "Valid commands:"
+  forM_ verbs $ \verb -> do
+    putStr $ name verb
+    putStr ": "
+    putStrLn $ brief $ help verb
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> printUsage >> exitFailure
+    (verbName : otherArgs) -> performVerb verbName otherArgs
---
+++ Snapshot.hs
@@ -0,0 +1,69 @@
+module Snapshot (takeSnapshot, Snapshotable(..)) where
+import Control.Exception (bracket, try, IOException)
+import Control.Monad (forM_)
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Time.Format (formatTime, defaultTimeLocale)
+import System.Directory (createDirectoryIfMissing, removeFile)
+import System.Exit (ExitCode (..))
+import System.FilePath ((</>), (<.>), splitFileName)
+import System.Posix.IO (openFd, closeFd, OpenFileFlags, defaultFileFlags, OpenMode (ReadOnly), exclusive)
+import System.Posix.Types (Fd)
+import System.Process (CreateProcess, proc, readCreateProcessWithExitCode)
+
+data Snapshotable =
+  Snapshotable
+  { subvolumePath :: FilePath
+  , snapshotBasePath :: FilePath
+  }
+
+snapshotProcess :: FilePath -> FilePath -> CreateProcess
+snapshotProcess sourcePath destinationPath =
+  proc "btrfs" ["subvolume", "snapshot", "-r", sourcePath, destinationPath]
+
+subpathForTime :: UTCTime -> FilePath
+subpathForTime time =
+  year </> month </> day </> timePart
+  where
+    format :: String -> String
+    format str = formatTime defaultTimeLocale str time
+    year = format "%Y"
+    month = format "%m"
+    day = format "%d"
+    timePart = format "%T"
+
+snapshotDestination :: Snapshotable -> UTCTime -> FilePath
+snapshotDestination snapshotable time = snapshotBasePath snapshotable </> subpathForTime time
+
+mkdirForSnapshotDestination :: FilePath -> IO ()
+mkdirForSnapshotDestination snapshotDestination = do
+  let (containingDir, target) = splitFileName snapshotDestination
+  createDirectoryIfMissing True containingDir
+
+lockfilePathForSnapshot :: FilePath -> FilePath
+lockfilePathForSnapshot snapshotPath = snapshotPath <.> "lock"
+
+withLockFile :: FilePath -> IO a -> IO (Either String a)
+withLockFile lockFile action = do
+  let flags = defaultFileFlags { exclusive = True }
+  let open = try $ openFd lockFile ReadOnly (Just 0) flags :: IO (Either IOException Fd)
+  let close eitherFd = case eitherFd of
+        Right fd -> closeFd fd >> removeFile lockFile
+        Left _ -> return ()
+  bracket open close $ \eitherFd -> case eitherFd of
+    Right _ -> do
+      value <- action
+      return $ Right value
+    Left _ -> return $ Left "Failed to create lockfile"
+
+takeSnapshot :: Snapshotable -> IO (Either String ())
+takeSnapshot snapshotable = do
+  now <- getCurrentTime
+  let destinationPath = snapshotDestination snapshotable now
+  let createProcess = snapshotProcess (subvolumePath snapshotable) destinationPath
+  let processAction = readCreateProcessWithExitCode createProcess ""
+  mkdirForSnapshotDestination destinationPath
+  result <- withLockFile (lockfilePathForSnapshot destinationPath) processAction
+  case result of
+    Left e -> return $ Left e
+    Right (ExitSuccess, _, _) -> return $ Right ()
+    Right (ExitFailure _, stdout, stderr) -> return $ Left stderr
